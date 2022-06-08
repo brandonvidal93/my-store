7af108860a3df727b98bf6da2d66e27dee8db16a
Brandon Vidal <brandonvidal93@gmail.com>
2022-06-08 16:35:40 -0500

    U - Manejo de estado

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
 import { Product } from '../models/product.model';
 
 @Injectable({
@@ -9,15 +11,21 @@ export class StoreService {
   /* Creating an empty array of type Product. */
   private myShoppingCart: Product[] = [];
 
+  /* This is a BehaviorSubject. It is a special type of Subject that keeps hold of the current value
+  and emits it to any new subscribers as soon as they subscribe. */
+  private myCart = new BehaviorSubject<Product[]>([]);
+  myCart$ = this.myCart.asObservable();
+
   constructor() { }
 
   /**
-   * The addProduct function takes a Product object as an argument and adds it to the myShoppingCart
-   * array
+   * We're pushing the product into the myShoppingCart array and then we're using the next() method to
+   * update the myCart subject with the new value of the myShoppingCart array
    * @param {Product} product - Product - this is the product that we are adding to the shopping cart.
    */
   addProduct(product: Product) {
     this.myShoppingCart.push(product);
+    this.myCart.next(this.myShoppingCart);
   }
 
   /**
