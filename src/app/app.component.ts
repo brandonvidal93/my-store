5b9585b1496fc4bd2f79c610bf0688ed9bf703d0
brandon.vidal <brandon.vidal@ceiba.com.co>
2023-10-04 11:12:56 -0500

    U - Auth User services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Product } from './models/product.model';
+
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +12,32 @@ export class AppComponent {
   imgParent = '';
   imgLoaded = '';
   showImg = true;
+
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {
+
+  }
+
+  createUser() {
+    this.usersService.create({
+      name: 'Brandon',
+      email: 'brandon@mail.com',
+      password: '12345'
+    })
+    .subscribe(data => {
+      console.log(data);
+      
+    });
+  }
+
+  login() {
+    this.authService.login('brandon@mail.com','12345')
+    .subscribe(data => {
+      console.log(data.access_token);
+    });
+  }
 
   /**
    * The function takes a string as an argument and sets the imgLoaded property to the value of the
