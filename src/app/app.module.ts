2360c33cb6b97e9c8cdfdf9d0f50ebbcd8395fd4
brandon.vidal <brandonvidal93@gmail.com>
2024-01-15 09:12:34 -0500

    U - Routing curso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,16 @@ import { HighlightDirective } from './directives/highlight.directive';
 import { TimeInterceptor } from './interceptors/time.interceptor';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 
+// Pages
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,7 +42,15 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
     ReversePipe,
     TimeAgoPipe,
     ChangeVocalesPipe,
-    HighlightDirective
+    HighlightDirective,
+    HomeComponent,
+    NotFoundComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
