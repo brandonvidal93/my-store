5ca37ddc4cb3d3ed32b79e7589c8828751a36289
brandon.vidal <brandonvidal93@gmail.com>
2024-01-18 09:03:39 -0500

    U - Refactor Callback hell

--- src/app/pages/category/category.component.ts
+++ src/app/pages/category/category.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+import { switchMap } from 'rxjs';
+
 import { Product } from 'src/app/models/product.model';
 
 import { ProductsService } from 'src/app/services/products.service';
@@ -24,16 +26,21 @@ export class CategoryComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
-      this.categoryId = params.get('id');
+    this.route.paramMap
+    .pipe(
+      switchMap(params => {
+        this.categoryId = params.get('id');
 
-      if(this.categoryId) {
-        this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
-        .subscribe(data => {
-          this.products = data;
-          this.offset += this.limit;
-        });
-      }
+        if(this.categoryId) {
+          return this.productsService.getByCategory(this.categoryId, this.limit, this.offset)
+        }
+
+        return [];
+      }),
+    )
+    .subscribe((data) => {
+      this.products = data;
+      this.offset += this.limit;
     });
   }
 
