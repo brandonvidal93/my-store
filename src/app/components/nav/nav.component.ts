6605880676634e14f82ef7c743aeaa4d5ec7e9e1
brandon.vidal <brandonvidal93@gmail.com>
2024-01-18 09:47:35 -0500

    Clase Router link & Router link active

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/category.model';
 import { User } from 'src/app/models/user.model';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { CategoriesService } from 'src/app/services/categories.service';
 import { StoreService } from 'src/app/services/store.service';
 
 @Component({
@@ -14,16 +16,20 @@ export class NavComponent implements OnInit {
   activeMenu = false;
   counter = 0;
   profile: User | null = null;
+  categories: Category[] = [];
 
   constructor(
     private authService: AuthService,
-    private storeService: StoreService
+    private storeService: StoreService,
+    private categoriesService: CategoriesService
   ) { }
 
   ngOnInit(): void {
     this.storeService.myCart$.subscribe(products => {
       this.counter = products.length;
     });
+
+    this.getAllCategories();
   }
 
   toggleMenu() {
@@ -35,6 +41,13 @@ export class NavComponent implements OnInit {
     .subscribe(user => {
       this.profile = user;
       console.log(user);
+    });
+  }
+
+  getAllCategories() {
+    this.categoriesService.getAll()
+    .subscribe(data => {
+      this.categories = data;
     });
   }
 }
